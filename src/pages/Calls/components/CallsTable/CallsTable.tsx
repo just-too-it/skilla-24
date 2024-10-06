84ad352a61ae429131b14762fe95b7ac57fe213c
Natasha <just.too.it@gmail.com>
2024-10-06 12:55:16 +0300

    edit ReadMe

--- src/pages/Calls/components/CallsTable/CallsTable.tsx
+++ src/pages/Calls/components/CallsTable/CallsTable.tsx
@@ -24,7 +24,7 @@ export const CallsTable = ({ calls }: CallsTableProps) => {
         estimation: getRandomRating(),
       }))
     );
-  }, []);
+  }, [calls]);
 
   return (
     <table className={styles.table}>
